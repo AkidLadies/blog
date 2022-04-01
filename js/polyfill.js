1c74f16a373b403990bae9921f2b6be07070c427
Akidst <luckychaoyue5@163.com>
2022-04-01 23:24:40 +0800

    Site updated: 2022-04-01 23:24:27

--- js/polyfill.js
+++ js/polyfill.js
@@ -1,3 +1,3 @@
-// build time:Fri Apr 01 2022 23:12:42 GMT+0800 (中国标准时间)
+// build time:Fri Apr 01 2022 23:24:24 GMT+0800 (中国标准时间)
 (function(o){}).call("object"===typeof window&&window||"object"===typeof self&&self||"object"===typeof global&&global||{});
 //rebuild by hrmmi 
