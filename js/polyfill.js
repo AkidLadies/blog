6465ab2815089f4a4384127299a8e86d798ed2c2
[六葉星璨] <[akiramerare@outlook.com]>
2022-05-15 18:36:53 +0800

    Site updated: 2022-05-15 18:36:49

--- js/polyfill.js
+++ js/polyfill.js
@@ -1,3 +1,3 @@
-// build time:Sun May 15 2022 18:18:44 GMT+0800 (中国标准时间)
+// build time:Sun May 15 2022 18:36:48 GMT+0800 (中国标准时间)
 (function(o){}).call("object"===typeof window&&window||"object"===typeof self&&self||"object"===typeof global&&global||{});
 //rebuild by hrmmi 
